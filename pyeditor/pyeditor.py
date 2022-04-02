23d0d7b9f32323eeb0f844bba9971e27f1e6858b
frank038 <42263206+frank038@users.noreply.github.com>
2022-04-02 19:38:02 +0200

    Add files via upload

--- pyeditor/pyeditor.py
+++ pyeditor/pyeditor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-# V 0.9.3
+# V 0.9.4
 import sys
 from PyQt5.QtWidgets import (qApp,QMainWindow,QStyleFactory,QWidget,QFileDialog,QSizePolicy,QFrame,QBoxLayout,QVBoxLayout,QHBoxLayout,QLabel,QPushButton,QApplication,QDialog,QMessageBox,QLineEdit,QComboBox,QCheckBox,QAction,QMenu,QStatusBar) 
 from PyQt5.QtCore import (Qt,pyqtSignal,QFile,QIODevice,QPoint)
 from PyQt5.QtGui import (QColor,QFont,QIcon,QPalette)
-from PyQt5.Qsci import (QsciLexerCustom,QsciScintilla,QsciLexerPython)
+from PyQt5.Qsci import (QsciLexerCustom,QsciScintilla,QsciLexerPython,QsciLexerBash,QsciLexerJavaScript)
 import os
 import re
 from cfgpyeditor import *
